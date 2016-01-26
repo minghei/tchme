ea159b77a81ec48bc836ce7d62c7e7fae3271d66
joseph81 <ming_hei@yhaoo.com>
2016-01-26 16:40:15 +0800

    file

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,17 @@
 angular.module('tchme').controller('mainController', function($scope) {
     $scope.message = 'Everyone come and see how good I look!';
+
+
+    $scope.uploadFile = function(){
+    	console.log(document.getElementById('fileToUpload').files);
+
+    	var reader = new FileReader();
+	    reader.onloadend = function(e){
+	    	var bytes = new Uint8Array(e.target.result);
+	    	console.log(bytes);
+	        console.log(e.target.result.byteLength);
+	    }
+	    reader.readAsArrayBuffer(document.getElementById('fileToUpload').files[0]);
+    };
 });
 
