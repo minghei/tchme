f7dfae3c6d1b666c3f50567eed2b2a97912b00ef
joseph <ming_hei@yahoo.com>
2016-02-02 00:15:22 +0800

    register student

--- public/js/registration.js
+++ public/js/registration.js
@@ -1,4 +1,4 @@
-angular.module('tchme').controller('registerController', function($scope, Data, Admin, User, $window) {
+angular.module('tchme').controller('registerController', function($scope, Data, Admin, User, Category, $window) {
 
     $scope.data = Data;
 
@@ -14,5 +14,56 @@ angular.module('tchme').controller('registerController', function($scope, Data, 
     	};
     	User.registerTutor(data); 	
     }
+
+
+    $scope.init = function(){
+        console.log("init");
+
+        $("#sdropdownGender").kendoDropDownList({
+          dataTextField: "name",
+          dataValueField: "id",
+          dataSource: [{id:1 ,name:'Male'},{ id:0, name: 'Female'}]
+        });
+
+        $("#sdropdownWorst").kendoDropDownList({
+          dataTextField: "subCategory",
+          dataValueField: "id",
+          dataSource: []
+        });
+
+        $("#sdropdownBest").kendoDropDownList({
+          dataTextField: "subCategory",
+          dataValueField: "id",
+          dataSource: []
+        });
+
+        $("#sdropdownLevel").kendoDropDownList({
+          dataTextField: "category",
+          dataValueField: "id",
+          dataSource: [],
+          select: function(e) {
+            setTimeout(function(){
+              $scope.srefresh();
+            },10);
+          }
+        });
+
+        Category.getCategory().then(function(results){
+          $("#sdropdownLevel").data("kendoDropDownList").dataSource.data(results.detail.detail);
+          $scope.srefresh();
+        });
+    }
+
+    $scope.srefresh = function(){
+          Category.getSubCategory($("#sdropdownLevel").data("kendoDropDownList").value()).then(function(results){
+            $("#sdropdownWorst").data("kendoDropDownList").dataSource.data(results.detail.detail);
+            $("#sdropdownBest").data("kendoDropDownList").dataSource.data(results.detail.detail);
+            $("#sdropdownBest").data("kendoDropDownList").select(0);
+            $("#sdropdownWorst").data("kendoDropDownList").select(0);
+            
+          });
+    }
+
+    $scope.init();
 });
 
